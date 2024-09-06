3be7bd049a3d4513ee3116beb3f5001837aefe92
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2024-09-06 13:43:50 +0000

    num&math

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,41 @@
+const score = 400 
+
+const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //precision value
+
+const otherNumber = 23.8966
+const otherNum = 123.8966
+const otherNumb = 1123.8966
+// console.log(otherNumber.toPrecision(3)); //rounding -
+// console.log(otherNum.toPrecision(3));
+// console.log(otherNumb.toPrecision(3));
+
+const num1 = 1000000
+// console.log(num1.toLocaleString());
+// console.log(num1.toLocaleString('en-IN')); //indian version
+
+//*************** Maths *********************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5)); //round off
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random()); //range 0-1 both inclusive
+// console.log((Math.random()*10) + 1); 
+// console.log(Math.floor((Math.random()*10)) + 1); 
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
