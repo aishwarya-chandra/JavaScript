297a52dce7c37c705fa1ffef650d1bfad142e2dd
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2025-01-17 13:52:25 +0000

    variables and constant

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,21 +1,26 @@
-const accountId = 144553
-let accountEmail = "chaish@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+const accountId = 144553               // constant , not changeable
+let accountEmail = "chaish@gmail.com"  // let -variable , changeable
+var accountPassword = "12345"          // var -variable , changeable
+accountCity = "Jaipur"                 // reserve memory without writing anything 
+                                       // tho not good practise
 
-// accountId = 2 // not allowed
+// only declared not initialized
+let accountState;                      // gives undefined if printed
+
+// accountId = 2           error
 
 accountEmail = "hc@hc.com"
 accountPassword = "121"
 accountCity = "Bengaluru"
 
 console.log(accountId);
+
+// to print in tabular structure
 console.table([accountEmail, accountPassword, accountCity, accountState])
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
-var have no control on scope, it chnages everywhere 
+var have no control on scope, it changes everywhere 
 if same variable name is used
  */
