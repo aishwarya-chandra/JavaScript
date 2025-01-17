6824271a90ab524d74a875587d85cc4349e2f727
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2025-01-17 15:11:47 +0000

    comparisons

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,12 +5,18 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+
+//DO NOT COMPARE DIFFERENT DATA TYPES
+//GIVES NON-PREDICTABLE RESULT
 // console.log("2">1);
 // console.log("02">1);
 
 // console.log(null > 0);
 // console.log(null == 0);
+
+//Comparison convert null to a number , treating it as 0
 // console.log(null >= 0);
+
 
 // console.log(undefined == 0);
 // console.log(undefined > 0);
