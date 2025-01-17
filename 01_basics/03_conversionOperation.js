169b464682ad6b55fb1775041a8d1831b58e039d
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2025-01-17 15:03:05 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,6 +7,8 @@ let score = false //1 for true & 0 for false
 // console.log(typeof score);
 // console.log(typeof(score));
 
+
+//converted in number
 let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
@@ -17,10 +19,14 @@ let valueInNumber = Number(score)
 // let isLogged = 14 //true
 // let isLogged = 0 //false
 
+
+//converted to boolean
 // let booleanIsLogged = Boolean(isLogged)
 // console.log(booleanIsLogged);
 //-------------------------------------------------------------
 let someNum = 33 //33 as string
+
+//converted to string
 let stringNum = String(someNum)
 
 // console.log(stringNum);
@@ -47,9 +53,11 @@ let str3 = str1 + str2
 
 // console.log("1" + 2);
 // console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);   //if string first then whole considered as string concatenation
+// console.log(1 + 2 + "2");   // pehle do math add then string concatenation
 
+
+//tricky conversion - not good for practise
 // console.log(+true);
 // console.log(+"");
 
@@ -61,7 +69,11 @@ let counter = 100
 // counter++;
 ++counter;
 console.log(counter);
+
 // prefix postfix js mdn on internet 
+// and 
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
