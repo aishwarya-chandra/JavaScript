313d3ac9cceb18d2ed8d801fcce78850269adbbb
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2025-01-20 16:46:56 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -4,6 +4,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
+// console.log(marvel_heros[3]);
 // console.log(marvel_heros[3][1]);
 
 // const allHeros = marvel_heros.concat(dc_heros)
@@ -20,9 +21,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("aishwarya"))
+console.log(Array.from("aishwarya"))
+console.log(Array.from({name: "aishwarya"})) // interesting
 
 let score1 = 100
 let score2 = 200
